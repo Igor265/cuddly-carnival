aa6502f1a00563fa31416fbc9331ac8df3b7a960
Igor Ribeiro <igorjfr@outlook.com>
2024-04-26 16:14:12 -0300

    HEY-11: adding like button to the interface

--- resources/views/components/question.blade.php
+++ resources/views/components/question.blade.php
@@ -2,6 +2,24 @@
     'question'
 ])
 
-<div class="rounded dark:bg-gray-800/50 shadow shadow-blue-500/50 p-3 dark:text-gray-400 bg-white overflow-hidden sm:rounded-lg text-gray-900">
-    {{ $question->question }}
+<div class="flex justify-between items-center rounded dark:bg-gray-800/50 shadow shadow-blue-500/50 p-3 dark:text-gray-400 bg-white overflow-hidden sm:rounded-lg text-gray-900">
+    <span>{{ $question->question }}</span>
+    <div>
+        <x-form
+            :action="route('question.like', $question->id)"
+        >
+            <button class="flex items-start space-x-2 text-green-500">
+                <x-icons.thumbs-up class="w-5 h-5 hover:text-yellow-300 cursor-pointer" />
+                <span>{{ $question->likes }}</span>
+            </button>
+        </x-form>
+        <x-form
+            :action="route('question.unlike', $question->id)"
+        >
+            <button class="flex items-start space-x-2 text-red-500">
+                <x-icons.thumbs-down class="w-5 h-5 hover:text-red-300 cursor-pointer" />
+                <span>{{ $question->unlikes }}</span>
+            </button>
+        </x-form>
+    </div>
 </div>
