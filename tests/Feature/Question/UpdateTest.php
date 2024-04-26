c08910aeeae97cd7c54134d21792644682def857
Igor Ribeiro <igorjfr@outlook.com>
2024-04-26 19:18:19 -0300

    HEY-16: it should update the question

---
+++ tests/Feature/Question/UpdateTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Models\{Question, User};
+
+use function Pest\Laravel\{actingAs, put};
+
+it('should update the question', function () {
+    // Arrange
+    $user     = User::factory()->create();
+    $question = Question::factory()
+        ->for($user, 'createdBy')
+        ->create(['draft' => true]);
+    actingAs($user);
+
+    // Act
+    put(route('question.update', $question->id), [
+        'question' => 'Updated question?',
+    ]);
+
+    $question->refresh();
+
+    // Assert
+    expect($question)->question->toBe('Updated question?');
+});
