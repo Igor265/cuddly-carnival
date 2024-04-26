46f44c7f114789d29b83ebfcff565c0705ba336a
Igor Ribeiro <igorjfr@outlook.com>
2024-04-26 16:31:11 -0300

    HEY-11: handling N+1 issue

--- resources/views/components/question.blade.php
+++ resources/views/components/question.blade.php
@@ -10,7 +10,7 @@
         >
             <button class="flex items-start space-x-2 text-green-500">
                 <x-icons.thumbs-up class="w-5 h-5 hover:text-yellow-300 cursor-pointer" />
-                <span>{{ $question->likes }}</span>
+                <span>{{ $question->votes_sum_like ?: 0 }}</span>
             </button>
         </x-form>
         <x-form
@@ -18,7 +18,7 @@
         >
             <button class="flex items-start space-x-2 text-red-500">
                 <x-icons.thumbs-down class="w-5 h-5 hover:text-red-300 cursor-pointer" />
-                <span>{{ $question->unlikes }}</span>
+                <span>{{ $question->votes_sum_unlike ?: 0 }}</span>
             </button>
         </x-form>
     </div>
